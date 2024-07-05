151aebbb7c754374a37b5bc53d8181bcbaed8641
ashton01L <ashtonamazing@gmail.com>
2024-07-05 13:27:02 -0500

    defined a function to convert the temp from C to F and used a while loop to ask for input and force validity

---
+++ main/temp_convert.py
@@ -0,0 +1,19 @@
+# Author: Ashton Lee
+# Github User: ashton01L
+# Date: 7/4/2024
+
+# Description: Asks the user for an input of a temperature in degrees Celsius
+# and converts it to degrees Fahrenheit, using the formula F = (9/5)C + 32
+
+def celsius_2_fahrenheit(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+while True:
+    try:
+        celsius = float(input("Please enter a Celsius temperature:\n"))
+        fahrenheit = celsius_2_fahrenheit(celsius)
+        print(f"The equivalent fahrenheit temperature is:\n {fahrenheit:.1f}")
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid input.")
